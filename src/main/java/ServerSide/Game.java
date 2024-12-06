031a191ef18e4d1acd17cfcc57119dae0487a1b0
ArvidUtas <arvidu@hotmail.com>
2024-12-06 15:02:28 +0100

    built game

--- src/main/java/ServerSide/Game.java
+++ src/main/java/ServerSide/Game.java
@@ -1,4 +1,26 @@
 package ServerSide;
 
-public class Game {
+public class Game extends Thread {
+    private Player player1;
+    private Player player2;
+
+    public Game(Player player1, Player player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+        this.player1.setOpponent(player2);
+        this.player2.setOpponent(player1);
+    }
+
+    public void run() {
+        try {
+            Protocol protocol = new Protocol(player1, player2);
+        } catch (Exception e) {
+            System.out.println("Exception in run method.");
+        } finally {
+            player1.closeConnection();
+            player2.closeConnection();
+        }
+    }
 }
+
+
