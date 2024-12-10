8b17e0139362d7b65f54d3ae2c97ca98d87136af
cfrank3N <adamcecarfrank@gmail.com>
2024-12-10 14:15:33 +0100

    Added abstract class Ship and subclasses of ship

---
+++ src/main/java/ClientSide/factory/Battleship.java
@@ -0,0 +1,7 @@
+package ClientSide.factory;
+
+public class Battleship extends Ship {
+    public Battleship(boolean isVertical) {
+        super(4, isVertical);
+    }
+}
---
+++ src/main/java/ClientSide/factory/Cruiser.java
@@ -0,0 +1,7 @@
+package ClientSide.factory;
+
+public class Cruiser extends Ship {
+    public Cruiser(boolean isVertical) {
+        super(3, isVertical);
+    }
+}
